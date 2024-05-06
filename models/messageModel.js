23af7eee5a5a69a3f47b61635a10a61cdc3aa6fc
devarashs <southernwavesofmadness@protonmail.com>
2024-05-06 20:20:19 +0330

    feat: Add message model and schema

---
+++ models/messageModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      maxlength: 1000,
+    },
+    attachments: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model("Message", messageSchema);
